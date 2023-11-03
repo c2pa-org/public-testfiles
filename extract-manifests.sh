d65cabeb16bede467d1c7be61da29cb4d61e0817
Rand McKinney <rmckinne@adobe.com>
2023-11-03 11:20:37 -0700

    Add detailed manifests to script and output

--- extract-manifests.sh
+++ extract-manifests.sh
@@ -10,9 +10,8 @@ find "$dir" -type f -print0 | while read -d $'\0' file; do
     echo "  - $(basename "$file")" >> "$output_file"
 done
 
-# Use c2patool to get any C2PA manifest data associated with images in /image/jpeg dir
-
-function get_all_manifests() {
+# Argument is name of asset file
+function extract_manifest() {
     if [ -f "$1" ]; then
 
         filename=$(basename -- "$1")
@@ -20,17 +19,21 @@ function get_all_manifests() {
         output_dir="$PWD/manifests/image/jpeg/$basefilename"
 
         echo "Running c2patool on: $basefilename"
-        #echo "RUNNING COMMAND: c2patool $1 -o $output_dir -f"
+        c2patool $1 -o $output_dir -f
 
+        echo "Running c2patool -d on: $basefilename"
+        output_dir="$PWD/detailed-manifests/image/jpeg/$basefilename"
         c2patool $1 -o $output_dir -f
     fi
 }
 
+# Use c2patool to get any C2PA manifest data associated with images in /image/jpeg dir
+
 for file in $PWD/image/jpeg/*; do
-    get_all_manifests "$file"
+    extract_manifest "$file"
 done
 
 for file in $PWD/video/mp4/*; do
-    get_all_manifests "$file"
+    extract_manifest "$file"
 done
 
