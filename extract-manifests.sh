0b97114fa13de66d688935beb330bf97f6809454
Rand McKinney <rmckinne@adobe.com>
2024-01-22 11:12:21 -0800

    Update READMEs

--- extract-manifests.sh
+++ extract-manifests.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Loop through all files in the directory and its subdirectories
-#find "$dir" -type f -print0 | while read -d $'\0' file; do
-#    echo "  - $(basename "$file")" >> "$output_file"
-#done
-
-
 # Use c2patool to get any C2PA manifest data associated with asset files
 # Note: c2patool must be installed and in the PATH
 # First argument is name of asset file, second argument is name of output directory
@@ -13,17 +7,12 @@ function extract_manifest() {
     if [ -f "$1" ]; then
         filename=$(basename -- "$1")
         basefilename="${filename%.*}"
-      
-        #output_dir="$2/manifests/$basefilename"
-        #echo "Running command: c2patool $1 -o $output_dir -f"
-        #c2patool $1 -o $output_dir -f
 
         output_dir="$2/manifests/$basefilename"
         echo "Running command: c2patool -d $1 -o $output_dir -f"
         c2patool -d $1 -o $output_dir -f 
     fi
 }
-
 
 # Iterate through directories containing asset files and call extract_manfiest 
 for dir in "video/mp4" "image/jpeg" "pdf"; do
